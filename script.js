8a59b9ad7dbc9dfa2721cf3ed7bb9e57bd3e4922
mariam2121 <112910300+mariam2121@users.noreply.github.com>
2023-12-03 17:47:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function shuffleOptions(groupId) {
-    const group = document.getElementById(groupId);
-    const options = Array.from(group.children);
-    options.shift(); // Elimina la opción "Categorias"
-    options.sort(() => Math.random() - 0.5);
-    group.innerHTML = "";
-    group.appendChild(document.createElement("option")); // Añade la opción "Categorias" nuevamente
-    options.forEach(option => group.appendChild(option));
-  }
 
-  // Llama a la función para reorganizar aleatoriamente las opciones de cada grupo
-  shuffleOptions("influencers-group");
-  shuffleOptions("cantantes-group");
-  shuffleOptions("actores-group");
